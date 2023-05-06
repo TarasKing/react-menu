f32fc06a100a60824678eea5feb708662363ddf5
TarasKing <ivanovtaras@live.ru>
2023-05-06 13:17:29 -0700

    start project menu

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <main>
+      <section className="menu">
+        <Title />
+      </section>
+    </main>
+  );
+}
+
+export default App;
